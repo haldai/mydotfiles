0f5818ce1fae148ae3aca6025eac4c5438e11cf8
haldai <dai.wzero@gmail.com>
2018-05-04 16:17:28 +0800

    flashes current active window by +BackSpace

---
+++ .i3/bin/flash-win.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# flashes the active window
+
+# require transset-df and a composite manager
+transset-df -a -m 0
+sleep .1
+transset-df -a -x 1
+sleep .1
+transset-df -a -m 0
+sleep .1
+transset-df -a -x 1
