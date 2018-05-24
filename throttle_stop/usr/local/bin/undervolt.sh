9713bedc995fb0272a06f492d63aca905a9c3f10
haldai <dai.wzero@gmail.com>
2018-05-25 00:16:32 +0800

    update

--- throttle_stop/usr/local/bin/undervolt.sh
+++ throttle_stop/usr/local/bin/undervolt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# undervolt settings
+# undervolt settings, require msr-tools from AUR
 /usr/bin/wrmsr 0x150 0x80000011EF600000 # cpu -130mV
 /usr/bin/wrmsr 0x150 0x80000111F6600000 # gpu -75mV
 /usr/bin/wrmsr 0x150 0x80000211F1E00000 # cache -110mV
