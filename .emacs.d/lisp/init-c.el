ccfd4fed18fd0335db8dbc9d98046d6de6b8dc4c
haldai <dai.wzero@gmail.com>
2019-11-04 18:08:13 +0000

    update emacs lsp setting

--- .emacs.d/lisp/init-c.el
+++ .emacs.d/lisp/init-c.el
@@ -23,6 +23,20 @@
     :diminish
     :init (modern-c++-font-lock-global-mode t)))
 
+;; C/C++/Objective-C support
+(use-package ccls
+  :straight t
+  :defines projectile-project-root-files-top-down-recurring
+  :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda ()
+                                                   (require 'ccls)
+                                                   (lsp)))
+  :config
+  (with-eval-after-load 'projectile
+    (setq projectile-project-root-files-top-down-recurring
+          (append '("compile_commands.json"
+                    ".ccls")
+                  projectile-project-root-files-top-down-recurring))))
+
 (provide 'init-c)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- .emacs.d/lisp/init-lsp.el
+++ .emacs.d/lisp/init-lsp.el
@@ -49,20 +49,6 @@
   :straight t
   :init (setq company-lsp-cache-candidates 'auto))
 
-;; C/C++/Objective-C support
-(use-package ccls
-  :straight t
-  :defines projectile-project-root-files-top-down-recurring
-  :hook ((c-mode c++-mode objc-mode cuda-mode) . (lambda ()
-                                                   (require 'ccls)
-                                                   (lsp)))
-  :config
-  (with-eval-after-load 'projectile
-    (setq projectile-project-root-files-top-down-recurring
-          (append '("compile_commands.json"
-                    ".ccls")
-                  projectile-project-root-files-top-down-recurring))))
-
 (provide 'init-lsp)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
