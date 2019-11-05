bc845702922cda95aeda89b3cb43a1133cb7221e
haldai <dai.wzero@gmail.com>
2019-11-05 15:21:18 +0000

    update .emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,6 +51,13 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (require 'init-utils)
 
+;;keep cursor at same position when scrolling
+(setq scroll-preserve-screen-position 1)
+
+;;scroll window up/down by one line
+(global-set-key (kbd "M-n") (kbd "C-u 1 C-v"))
+(global-set-key (kbd "M-p") (kbd "C-u 1 M-v"))
+
 ;;----------------------------------------------------------------------------
 ;; Load configs for specific features and modes
 ;;----------------------------------------------------------------------------
