c4003a4cc40c07439f5e8c5f2c97a56814baddfc
Wang-Zhou Dai <dai.wzero@gmail.com>
2020-06-01 20:43:31 +0100

    python language server

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -13,14 +13,25 @@
   :straight t
   :ensure nil
   :defines gud-pdb-command-name pdb-path
-  :hook (python-mode . lsp-mode)
   :init
   (setq python-indent-guess-indent-offset t)
   (setq python-indent-guess-indent-offset-verbose nil)
   :config
 
+  ;; language server
+  (use-package lsp-python-ms
+    :straight t
+    :ensure t
+    :init (setq lsp-python-ms-auto-install-server t)
+    :hook (python-mode . (lambda ()
+                           (require 'lsp-python-ms)
+                           (lsp))))  ; or lsp-deferred
+
   ;; Live Coding in Python
-  (use-package live-py-mode :straight t))
+  (use-package live-py-mode :straight t)
+  (use-package py-autopep8
+    :straight t
+    :hook (python-mode . py-autopep8-enable-on-save)))
 
 (provide 'init-python)
 
