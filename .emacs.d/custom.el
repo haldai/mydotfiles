d0479301d536a5858d478b75d333d0eedf56b589
Wang-Zhou Dai <dai.wzero@gmail.com>
2020-07-01 13:29:24 +0100

    update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -synctex=1")
  '(TeX-command-list
-   (quote
-    (("TeX" "%(PDF)%(tex) %(file-line-error) %(extraopts) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+   '(("TeX" "%(PDF)%(tex) %(file-line-error) %(extraopts) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
       (plain-tex-mode texinfo-mode ams-tex-mode)
       :help "Run plain TeX")
      (#("LaTeX" 0 1
@@ -90,37 +89,35 @@
         (idx 17))
       "xelatex%(mode) -synctex=1 %t" TeX-run-TeX nil
       (latex-mode doctex-mode)
-      :help "XeLaTex for CJK languages"))))
- '(TeX-source-correlate-method (quote synctex))
+      :help "XeLaTex for CJK languages")))
+ '(TeX-source-correlate-method 'synctex)
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server nil)
  '(TeX-view-program-list
-   (quote
-    (("Acrobat" "Acrobat.exe %o")
+   '(("Acrobat" "Acrobat.exe %o")
      ("Gsview" "gsview32.exe %o")
      ("gv" "gv %o")
      ("qpdfview" "qpdfview --unique %o")
      ("MasterPDF" "masterpdfeditor4 %o")
      ("Evince" "evince %o")
      ("Firefox" "firefox %o")
-     ("Yap" "yap.exe %o"))))
- '(TeX-view-program-selection (quote ((output-pdf "PDF Tools") (output-dvi "gv"))))
+     ("Yap" "yap.exe %o")))
+ '(TeX-view-program-selection '((output-pdf "PDF Tools") (output-dvi "gv")))
  '(ediprolog-program "swipl")
  '(enable-recursive-minibuffers t)
- '(epa-pinentry-mode (quote loopback))
+ '(epa-pinentry-mode 'loopback)
  '(global-auto-revert-non-file-buffers t)
- '(ido-create-new-buffer (quote always))
+ '(ido-create-new-buffer 'always)
  '(ido-enable-flex-matching t)
  '(ido-save-directory-list-file "/home/daiwz/.emacs.d/.cache/ido.last")
  '(ido-vertical-mode t)
  '(ivy-use-virtual-buffers t)
- '(markdown-coding-system (quote utf-8))
+ '(markdown-coding-system 'utf-8)
  '(markdown-command "pandoc -f markdown -t html")
  '(prolog-electric-if-then-else-flag t)
  '(prolog-paren-indent-p t)
  '(prolog-program-name
-   (quote
-    (((getenv "EPROLOG")
+   '(((getenv "EPROLOG")
       (eval
        (getenv "EPROLOG")))
      (eclipse "eclipse")
@@ -128,5 +125,24 @@
      (sicstus "sicstus")
      (swi "pl")
      (gnu "gprolog")
-     (t "prolog"))))
- '(prolog-program-switches (quote ((swi ("-G128M" "-T128M" "-L128M" "-O")) (t nil)))))
+     (t "prolog")))
+ '(prolog-program-switches '((swi ("-G128M" "-T128M" "-L128M" "-O")) (t nil)))
+ '(pyim-dicts
+   '((:name "bigdict" :file "/home/daiwz/.emacs.d/pyim-bigdict.pyim.gz"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(all-the-icons-dired-dir-face ((t (:foreground nil))))
+ '(all-the-icons-dsilver ((((background dark)) :foreground "#838484") (((background light)) :foreground "#838484")))
+ '(all-the-icons-lsilver ((((background dark)) :foreground "#B9B6AA") (((background light)) :foreground "#7F7869")))
+ '(all-the-icons-silver ((((background dark)) :foreground "#716E68") (((background light)) :foreground "#716E68")))
+ '(diff-hl-change ((t (:background "#46D9FF"))))
+ '(diff-hl-delete ((t (:background "#ff6c6b"))))
+ '(diff-hl-insert ((t (:background "#98be65"))))
+ '(git-timemachine-minibuffer-author-face ((t (:inherit font-lock-string-face))))
+ '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+ '(hl-todo ((t (:box t :inherit))))
+ '(lsp-ui-doc-background ((t (:background nil))))
+ '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
