cae8180bc76213b9d3d49c95033d6b045eebc5b4
Wang-Zhou Dai <dai.wzero@gmail.com>
2020-07-07 01:13:31 +0100

    update

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -158,26 +158,26 @@ beautiful.init(awful.util.getdir("config") .. "/themes/zenburn/theme.lua")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 -- awful.util.tagnames = { "壹", "貳", "叄", "肆", "伍", "陸", "柒", "捌", "玖" }
-awful.util.tagnames = { "☰", "☱", "☲", "☳", "☴", "☵", "☶", "☷", "☯" }
+-- awful.util.tagnames = { "☰", "☱", "☲", "☳", "☴", "☵", "☶", "☷", "☯" }
 awful.layout.append_default_layouts({
       awful.layout.suit.tile,
-   -- awful.layout.suit.tile.left,
-   -- awful.layout.suit.tile.bottom,
-   -- awful.layout.suit.tile.top,
-   -- awful.layout.suit.fair,
-   lain.layout.centerwork,
-   lain.layout.termfair,
-   awful.layout.suit.fair.horizontal,
-   -- awful.layout.suit.spiral,
-   -- awful.layout.suit.spiral.dwindle,
-   -- awful.layout.suit.max,
-   -- awful.layout.suit.max.fullscreen,
-   -- awful.layout.suit.magnifier,
-   awful.layout.suit.corner.nw,
-   awful.layout.suit.corner.ne,
-   -- awful.layout.suit.corner.sw,
-   -- awful.layout.suit.corner.se,
-   awful.layout.suit.floating})
+      -- awful.layout.suit.tile.left,
+      -- awful.layout.suit.tile.bottom,
+      -- awful.layout.suit.tile.top,
+      -- awful.layout.suit.fair,
+      lain.layout.centerwork,
+      lain.layout.termfair,
+      awful.layout.suit.fair.horizontal,
+      -- awful.layout.suit.spiral,
+      -- awful.layout.suit.spiral.dwindle,
+      -- awful.layout.suit.max,
+      -- awful.layout.suit.max.fullscreen,
+      -- awful.layout.suit.magnifier,
+      awful.layout.suit.corner.nw,
+      awful.layout.suit.corner.ne,
+      -- awful.layout.suit.corner.sw,
+      -- awful.layout.suit.corner.se,
+      awful.layout.suit.floating})
 -- }}}
 
 -- {{{ Menu
