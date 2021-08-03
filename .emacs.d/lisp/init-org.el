ebb9e58b87b357fa258ec7688100a1adac4b46e6
Wang-Zhou Dai <dai.wzero@gmail.com>
2021-08-03 12:48:51 +0800

    global keycast mode and org-roam v2

--- .emacs.d/lisp/init-org.el
+++ .emacs.d/lisp/init-org.el
@@ -17,6 +17,37 @@
   :config
   (use-package org-contrib :straight t)
 
+  (use-package org-roam
+    :straight t
+    :ensure t
+    :custom
+    (org-roam-directory (file-truename "~/Org"))
+    :bind (("C-c n l" . org-roam-buffer-toggle)
+           ("C-c n f" . org-roam-node-find)
+           ("C-c n g" . org-roam-graph)
+           ("C-c n i" . org-roam-node-insert)
+           ("C-c n c" . org-roam-capture)
+           ;; Dailies
+           ("C-c n j" . org-roam-dailies-capture-today))
+    :config
+    (org-roam-setup)
+    ;; If using org-roam-protocol
+    (require 'org-roam-protocol))
+
+  (use-package org-roam-ui
+    :straight (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+    :after org-roam
+    :hook
+    ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+    ;;         a hookable mode anymore, you're advised to pick something yourself
+    ;;         if you don't care about startup time, use
+    ;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
   ;; latex preview scale
   (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.2))
 
