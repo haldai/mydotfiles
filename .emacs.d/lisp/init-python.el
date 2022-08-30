fc0170f7c0e3cd9ed7fa873e8485c670f5926177
Wang-Zhou Dai <dai.wzero@gmail.com>
2022-08-30 13:25:29 +0800

    change lsp-python-ms to lsp-pyright

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -18,21 +18,19 @@
   (setq python-indent-guess-indent-offset-verbose nil)
   :config
 
+  ;; language server
+  (use-package lsp-pyright
+    :ensure t
+    :straight t
+    :hook (python-mode . (lambda ()
+                           (require 'lsp-pyright)
+                           (lsp-deferred))))  ; or lsp-deferred
+
   ;; Live Coding in Python
   (use-package live-py-mode :straight t)
   (use-package py-autopep8
     :straight t
-    :hook (python-mode . py-autopep8-enable-on-save))
-
-  ;; lsp-pyright
-  (use-package lsp-pyright
-    :straight t
-    :ensure t
-    :hook (python-mode . (lambda ()
-                           (require 'lsp-pyright)
-                           (lsp))))  ; or lsp-deferred
-
-  )
+    :hook (python-mode . py-autopep8-enable-on-save)))
 
 (provide 'init-python)
 
