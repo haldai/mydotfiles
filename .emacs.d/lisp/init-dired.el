b8884d0ed24ed973eb2e31509c0470168087f32d
Wang-Zhou Dai <dai.wzero@gmail.com>
2022-09-12 14:01:55 +0800

    add fzf

--- .emacs.d/lisp/init-dired.el
+++ .emacs.d/lisp/init-dired.el
@@ -13,7 +13,7 @@
   (setq dired-recursive-copies 'always)
 
   (when (executable-find "gls")
-      ;; Use GNU ls as `gls' from `coreutils' if available.
+    ;; Use GNU ls as `gls' from `coreutils' if available.
     (setq insert-directory-program "gls"))
 
   (when (executable-find "ls")
@@ -94,6 +94,26 @@
     (setq dired-omit-files
           (concat dired-omit-files
                   "\\|^.DS_Store$\\|^.projectile$\\|^.git*\\|^.svn$\\|^.vscode$\\|\\.js\\.meta$\\|\\.meta$\\|\\.elc$\\|^.emacs.*"))))
+
+(use-package fzf
+  :straight t
+  :bind
+  ;; Don't forget to set keybinds!
+  ("C-c C-z z" . fzf)
+  ("C-c C-z f" . fzf-find-file)
+  ("C-c C-z d" . fzf-find-file-in-dir)
+  ("C-c C-z r" . fzf-recentf)
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        ;; fzf/grep-command "rg --no-heading -nH"
+        fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
 
 (provide 'init-dired)
 
