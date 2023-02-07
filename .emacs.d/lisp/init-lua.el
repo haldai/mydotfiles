eb422415e09c9e296e3deb6080c20613cfb198a7
Wang-Zhou Dai <dai.wzero@gmail.com>
2023-02-07 20:04:11 +0800

    update

--- .emacs.d/lisp/init-lua.el
+++ .emacs.d/lisp/init-lua.el
@@ -11,6 +11,9 @@
   :mode ("\\.lua\\'" . lua-mode)
   :interpreter ("lua" . lua-mode))
 
+(use-package pico8-mode
+  :straight (pico8-mode :type git :host github :repo "Kaali/pico8-mode"))
+
 (provide 'init-lua)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
