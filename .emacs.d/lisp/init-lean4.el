b759d110ad7e0208c8e5d4ca42bf0138699a207b
Wang-Zhou Dai <dai.wzero@gmail.com>
2025-02-21 14:51:02 +0800

    update reveal

--- .emacs.d/lisp/init-lean4.el
+++ .emacs.d/lisp/init-lean4.el
@@ -6,13 +6,10 @@
 ;;; Code:
 
 (use-package lean4-mode
-  :straight (lean4-mode
-         :type git
-         :host github
-         :repo "leanprover/lean4-mode"
-         :files ("*.el" "data"))
-  ;; to defer loading the package until required
-  :commands (lean4-mode))
+  :commands lean4-mode
+  :straight (lean4-mode :type git :host github
+                        :repo "leanprover-community/lean4-mode"
+                        :files ("*.el" "data")))
 
 (provide 'init-lean4)
 
